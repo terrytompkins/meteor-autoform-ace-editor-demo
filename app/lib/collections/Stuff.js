983e7857deabbff5dcc4dddb18ac14f152314f1b
Terry Tompkins <ttompkins@Terrys-MacBook-Pro.local>
2016-03-29 21:57:06 -0400

    template edits to customize default header/footer/home

--- app/lib/collections/Stuff.js
+++ app/lib/collections/Stuff.js
@@ -3,31 +3,31 @@ stuff = "Stuff";  // avoid typos, this string occurs many times.
 Stuff = new Mongo.Collection(stuff);
 
 Meteor.methods({
-  /**
-   * Invoked by AutoForm to add a new Stuff record.
-   * @param doc The Stuff document.
-   */
-  addStuff: function(doc) {
-    check(doc, Stuff.simpleSchema());
-    Stuff.insert(doc);
-  },
-  /**
-   *
-   * Invoked by AutoForm to update a Stuff record.
-   * @param doc The Stuff document.
-   * @param docID It's ID.
-   */
-  editStuff: function(doc, docID) {
-    check(doc, Stuff.simpleSchema());
-    Stuff.update({_id: docID}, doc);
-  }
+    /**
+     * Invoked by AutoForm to add a new Stuff record.
+     * @param doc The Stuff document.
+     */
+    addStuff: function (doc) {
+        check(doc, Stuff.simpleSchema());
+        Stuff.insert(doc);
+    },
+    /**
+     *
+     * Invoked by AutoForm to update a Stuff record.
+     * @param doc The Stuff document.
+     * @param docID It's ID.
+     */
+    editStuff: function (doc, docID) {
+        check(doc, Stuff.simpleSchema());
+        Stuff.update({_id: docID}, doc);
+    }
 });
 
 // Publish the entire Collection.  Subscription performed in the router.
 if (Meteor.isServer) {
-  Meteor.publish(stuff, function () {
-    return Stuff.find();
-  });
+    Meteor.publish(stuff, function () {
+        return Stuff.find();
+    });
 }
 
 
@@ -37,37 +37,52 @@ if (Meteor.isServer) {
  * See: https://github.com/aldeed/meteor-autoform#affieldinput
  */
 Stuff.attachSchema(new SimpleSchema({
-  name: {
-    label: "Name",
-    type: String,
-    optional: false,
-    max: 20,
-    autoform: {
-      group: stuff,
-      placeholder: "Bicycle"
+    name: {
+        label: "Name",
+        type: String,
+        optional: false,
+        max: 20,
+        autoform: {
+            group: stuff,
+            placeholder: "Bicycle"
+        }
+    },
+    quantity: {
+        label: "Quantity",
+        type: Number,
+        optional: false,
+        autoform: {
+            group: stuff,
+            placeholder: "3"
+        }
+    },
+    notes: {
+        type: String,
+        label: "Notes",
+        autoform: {
+            group: stuff,
+            afFieldInput: {
+                type: "ace-editor",
+                editorWidth: "600px",
+                editorHeight: "250px",
+                theme: "twilight",
+                mode: "text"
+            }
+        }
     }
-  },
-  quantity: {
-    label: "Quantity",
-    type: Number,
-    optional: false,
-    autoform: {
-      group: stuff,
-      placeholder: "3"
+    ,
+    jsonEditor: {
+        type: String,
+        label: "JSON Editor",
+        autoform: {
+            group: stuff,
+            afFieldInput: {
+                type: "ace-editor",
+                editorWidth: "600px",
+                editorHeight: "250px",
+                theme: "dawn",
+                mode: "json"
+            }
+        }
     }
-  },
-  notes: {
-    type: String,
-    label: "Notes",
-    autoform: {
-      group: stuff,
-      afFieldInput: {
-        type: "ace-editor",
-        editorWidth: "600px",
-        editorHeight: "250px",
-        theme: "twilight",
-        mode: "text"
-      }
-    }
-  }
 }));
